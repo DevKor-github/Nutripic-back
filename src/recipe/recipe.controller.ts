c2d7e3803d8808f50028cbe2561cf5ed143616c4
whsung0330 <whsung0330@gmail.com>
2024-11-03 17:18:07 +0900

    chore: update prisma schema

--- src/recipe/recipe.controller.ts
+++ src/recipe/recipe.controller.ts
@@ -4,4 +4,10 @@ import { RecipeService } from './recipe.service';
 @Controller('recipe')
 export class RecipeController {
   constructor(private readonly recipeService: RecipeService) {}
+
+  //레시피 추천 (현재 식재료 기반)
+
+  //레시피 검색 (식재료?)
+
+  //레시피 북마크
 }
