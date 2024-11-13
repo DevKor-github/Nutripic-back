98c0e14e83175e7aeb01508b820a607fb3c8577a
whsung0330 <whsung0330@gmail.com>
2024-11-14 00:05:25 +0900

    chore:food schema update

--- src/recipe/recipe.service.ts
+++ src/recipe/recipe.service.ts
@@ -4,4 +4,35 @@ import { RecipeRepository } from './recipe.respository';
 @Injectable()
 export class RecipeService {
   constructor(private readonly recipeRepository: RecipeRepository) {}
+
+  getRecommandedRecipe(
+    uid: string
+  ): Promise<import('./dto/recipe.dto').RecipeDto[]> {
+    throw new Error('Method not implemented.');
+
+    //현재 만들 수 있음 / 재료 더 필요함 리스트 분리
+    //완전 랜덤
+    //알레르기 식품 제외 --> 유저 테이블 수정 필요
+
+    //최초 3개 따로 분류
+  }
+
+  getFilteredRecipe(
+    uid: string,
+    ingredients: import('./dto/ingredient.dto').ingredientDto[]
+  ): Promise<import('./dto/recipe.dto').RecipeDto[]> {
+    throw new Error('Method not implemented.');
+    //필터링 기준
+    //식재료
+    //난이도
+  }
+
+  addRecipeBookmark(uid: string, recipeId: number): Promise<number> {
+    //bookmark
+  }
+
+  //excludeAllergic()
+
+  //TODO 식품 카테고리 분류
+  //TODO 식품 수량 트래킹 X
 }
