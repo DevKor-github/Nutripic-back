992fdf396f6d51dbd7420fdc6ab22039dd56a628
wonjuneee <tony000709@gmail.com>
2024-11-28 13:53:35 +0900

    docs: swagger documentation

--- src/diary/diary.controller.ts
+++ src/diary/diary.controller.ts
@@ -18,8 +18,23 @@ import { GetAllDiaryResDto } from './dto/getAllDiary.dto';
 import { GetDiaryResDto } from './dto/getDiary.dto';
 import { CreateDiaryReqDto } from './dto/createDiary.dto';
 import { UpdateDiaryReqDto, UpdateDiaryResDto } from './dto/updateDiary.dto';
+import {
+  ApiBadGatewayResponse,
+  ApiBody,
+  ApiConsumes,
+  ApiCreatedResponse,
+  ApiExtraModels,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiParam,
+  ApiTags,
+} from '@nestjs/swagger';
+import { Public } from 'src/auth/auth.guard';
+import { $Enums } from '@prisma/client';
 
-// @Public()
+@Public()
+@ApiTags('Diary')
 @Controller('diary')
 export class DiaryController {
   constructor(
@@ -38,6 +53,17 @@ export class DiaryController {
     return 'Diary API Test';
   }
 
+  @ApiOperation({ summary: '특정 유저의 모든 다이어리 조회' })
+  @ApiParam({
+    name: 'index',
+    required: true,
+    description: '현재 월을 기준으로 캘린더를 앞뒤로 넘긴 횟수',
+    example: 0,
+  })
+  @ApiOkResponse({
+    type: GetAllDiaryResDto,
+    isArray: true,
+  })
   @Get('calendar/:index')
   @HttpCode(HttpStatus.OK)
   getAllDiary(
@@ -48,6 +74,19 @@ export class DiaryController {
     return this.diaryService.getDiaryByUser(uid, index);
   }
 
+  @ApiOperation({ summary: '특정 다이어리 조회' })
+  @ApiParam({
+    name: 'diaryId',
+    required: true,
+    description: '특정 다이어리의 ID',
+    example: 7,
+  })
+  @ApiOkResponse({
+    type: GetDiaryResDto,
+  })
+  @ApiNotFoundResponse({
+    description: '해당 ID의 다이어리가 존재하지 않습니다.',
+  })
   @Get('/:diaryId')
   @HttpCode(HttpStatus.OK)
   getDiary(
@@ -57,6 +96,18 @@ export class DiaryController {
     return this.diaryService.getDiaryById(diaryId);
   }
 
+  @ApiOperation({ summary: '다이어리 생성' })
+  @ApiConsumes('application/json')
+  @ApiBody({
+    description: '다이어리 본문, 등록일자, 이미지 URL',
+    type: CreateDiaryReqDto,
+  })
+  @ApiCreatedResponse({
+    description: '다이어리 생성 성공',
+  })
+  @ApiBadGatewayResponse({
+    description: '다이어리 생성 실패',
+  })
   @Post('/add')
   @HttpCode(HttpStatus.CREATED)
   async createDiary(
@@ -68,6 +119,38 @@ export class DiaryController {
     return this.diaryService.createDiary(uid, createDiaryReqDto);
   }
 
+  @ApiOperation({ summary: '다이어리 수정' })
+  @ApiParam({
+    name: 'diaryId',
+    required: true,
+    description: '특정 다이어리의 ID',
+    example: 7,
+  })
+  @ApiBody({
+    schema: {
+      properties: {
+        body: {
+          type: 'string',
+          example: '본문 예시입니다.',
+        },
+        date: {
+          type: 'Date || String',
+          example: '2024-11-12T19:30:00.000Z',
+        },
+      },
+      type: $Enums['UpdateDiaryReqDto'],
+    },
+  })
+  @ApiOkResponse({
+    description: '다이어리 수정 성공',
+    type: UpdateDiaryResDto,
+  })
+  @ApiNotFoundResponse({
+    description: '해당 ID의 다이어리가 존재하지 않습니다.',
+  })
+  @ApiBadGatewayResponse({
+    description: '다이어리 수정 실패',
+  })
   @Patch('/update/:diaryId')
   @HttpCode(HttpStatus.OK)
   updateDiary(
@@ -78,6 +161,17 @@ export class DiaryController {
     return this.diaryService.updateDiary(diaryId, updateDiaryReqDto);
   }
 
+  @ApiOperation({ summary: '이미지 업로드 presigned URL 생성' })
+  @ApiParam({
+    name: 'fileName',
+    required: true,
+    description: '업로드할 이미지 파일명',
+    example: 'example_image.jpg',
+  })
+  @ApiOkResponse({
+    description: '이미지 업로드 presigned URL 생성 성공',
+    type: String,
+  })
   @Get('/get-signed-url/:fileName')
   @HttpCode(HttpStatus.OK)
   getSignedUrl(@Param() fileName: string): Promise<string> {
@@ -85,6 +179,19 @@ export class DiaryController {
     return this.awsService.getPresignedUrl(fileName);
   }
 
+  @ApiOperation({ summary: '다이어리 삭제' })
+  @ApiParam({
+    name: 'diaryId',
+    required: true,
+    description: '특정 다이어리의 ID',
+    example: 7,
+  })
+  @ApiOkResponse({
+    description: '다이어리 삭제 성공',
+  })
+  @ApiNotFoundResponse({
+    description: '해당 ID의 다이어리가 존재하지 않습니다.',
+  })
   @Delete('/delete/:diaryId')
   @HttpCode(HttpStatus.NO_CONTENT)
   async deleteDiary(
@@ -95,6 +202,20 @@ export class DiaryController {
     return this.diaryService.deleteDiary(diaryId);
   }
 
+  @ApiOperation({ summary: '다이어리 복원' })
+  @ApiParam({
+    name: 'diaryId',
+    required: true,
+    description: '특정 다이어리의 ID',
+    example: 7,
+  })
+  @ApiOkResponse({
+    description: '다이어리 복원 성공',
+    type: UpdateDiaryResDto,
+  })
+  @ApiNotFoundResponse({
+    description: '해당 ID의 다이어리가 존재하지 않습니다.',
+  })
   @Patch('/restore/:diaryId')
   @HttpCode(HttpStatus.OK)
   async restoreDiary(
--- src/diary/dto/createDiary.dto.ts
+++ src/diary/dto/createDiary.dto.ts
@@ -1,13 +1,28 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsDateString, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateDiaryReqDto {
+  @ApiProperty({
+    description: '다이어리 본문',
+    example: '본문 예시입니다.',
+  })
   @IsString()
   @IsNotEmpty()
   body: string;
 
+  @ApiProperty({
+    description: '원하는 다이어리 등록 일자',
+    example: new Date('2024-11-27T19:30:00.000Z'),
+    default: new Date(),
+    type: Date || String,
+  })
   @IsDateString()
   date: Date | string;
 
+  @ApiProperty({
+    description: '업로드 이미지 객체 URL',
+    example: 'https://example.com',
+  })
   @IsString()
   @IsNotEmpty()
   url: string;
