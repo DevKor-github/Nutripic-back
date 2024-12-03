638fd51454df19abddb78a35e4876b83b611ecb0
EC2 Default User <ec2-user@ip-172-30-2-95.ap-northeast-2.compute.internal>
2024-12-03 07:09:16 +0000

    chore:ec2 environment setup

--- src/main.ts
+++ src/main.ts
@@ -50,7 +50,7 @@ async function bootstrap() {
     document
   );
 
-  await app.listen(configService.get('SERVER_PORT'));
+  await app.listen(configService.get('SERVER_PORT'), '0.0.0.0');
 
   const logger = new Logger('bootstrap');
   logger.log(`Application is running on: ${await app.getUrl()}`);
