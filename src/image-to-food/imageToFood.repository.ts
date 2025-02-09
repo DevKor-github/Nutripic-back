b6295b48afecf9a401ec7f85ae2be80165941533
whsung0330 <whsung0330@gmail.com>
2025-02-09 17:27:17 +0900

    fix: 유통기한 날짜 계산 오류 수정

--- src/image-to-food/imageToFood.repository.ts
+++ src/image-to-food/imageToFood.repository.ts
@@ -18,9 +18,8 @@ export class ImageToFoodRepository {
     const foodInfoMap = new Map<string, CreateFoodDto>();
     (await foodInfo).forEach(async (food) => {
       const currentDate = new Date();
-      const expireDate = new Date(
-        currentDate.setDate(currentDate.getDate() + food.expireDate)
-      );
+      const expireDate = new Date(currentDate);
+      expireDate.setDate(expireDate.getDate() + food.expireDate);
       foodInfoMap.set(food.class2, {
         name: food.class2,
         storageType: food.storageType,
