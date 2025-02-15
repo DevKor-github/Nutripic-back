9c1df1dc5ea3344d144b60a0349258815ba27692
whsung0330 <whsung0330@gmail.com>
2025-02-15 21:56:55 +0900

    fix: change param to query

--- src/log/log.controller.ts
+++ src/log/log.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, Get, Param, Res, StreamableFile } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Query,
+  Res,
+  StreamableFile,
+} from '@nestjs/common';
 import { LogService } from './log.service';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { Public } from 'src/auth/auth.guard';
@@ -9,17 +16,24 @@ import { string } from 'joi';
 export class LogController {
   constructor(private readonly logService: LogService) {}
 
+  @Public()
+  @Get('/test')
+  getTest() {
+    console.log('test');
+    return 'test';
+  }
+
   @ApiOperation({ summary: '로그 파일 확인' })
   @Public()
   @Get()
-  getLog(@Param('pw') pw: string): StreamableFile {
+  getLog(@Query('pw') pw: string): StreamableFile {
     return this.logService.getLogs(pw);
   }
 
   @ApiOperation({ summary: '에러 로그 확인' })
   @Public()
   @Get('/error')
-  getErrorLog(@Param('pw') pw: string): StreamableFile {
+  getErrorLog(@Query('pw') pw: string): StreamableFile {
     return this.logService.getErrorLogs(pw);
   }
 }
--- src/log/log.service.ts
+++ src/log/log.service.ts
@@ -16,7 +16,7 @@ export class LogService {
     this.errorLogFilePath = this.configService.get('ERROR_LOG_FILE_PATH');
   }
   getLogs(pw: string): StreamableFile {
-    if (pw != this.configService.get('LOG_PW')) {
+    if (pw !== this.configService.get('LOG_PW')) {
       throw new UnauthorizedException('로그 파일을 확인할 권한이 없습니다.');
     }
     if (!existsSync(this.logFilePath)) {
