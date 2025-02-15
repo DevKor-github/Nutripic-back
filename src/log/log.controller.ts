3528004657c0f08b947c5950d5ee6b8b2a4fc04d
whsung0330 <whsung0330@gmail.com>
2025-02-15 21:44:07 +0900

    mod: add pw to log api

--- src/log/log.controller.ts
+++ src/log/log.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Res, StreamableFile } from '@nestjs/common';
+import { Controller, Get, Param, Res, StreamableFile } from '@nestjs/common';
 import { LogService } from './log.service';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { Public } from 'src/auth/auth.guard';
+import { string } from 'joi';
 
 @ApiTags('Log')
 @Controller('log')
@@ -11,14 +12,14 @@ export class LogController {
   @ApiOperation({ summary: '로그 파일 확인' })
   @Public()
   @Get()
-  getLog(): StreamableFile {
-    return this.logService.getLogs();
+  getLog(@Param('pw') pw: string): StreamableFile {
+    return this.logService.getLogs(pw);
   }
 
   @ApiOperation({ summary: '에러 로그 확인' })
   @Public()
   @Get('/error')
-  getErrorLog(): StreamableFile {
-    return this.logService.getErrorLogs();
+  getErrorLog(@Param('pw') pw: string): StreamableFile {
+    return this.logService.getErrorLogs(pw);
   }
 }
--- src/log/log.service.ts
+++ src/log/log.service.ts
@@ -1,4 +1,9 @@
-import { Injectable, NotFoundException, StreamableFile } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  StreamableFile,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { createReadStream, existsSync } from 'fs';
 
@@ -10,7 +15,10 @@ export class LogService {
     this.logFilePath = this.configService.get('LOG_FILE_PATH');
     this.errorLogFilePath = this.configService.get('ERROR_LOG_FILE_PATH');
   }
-  getLogs(): StreamableFile {
+  getLogs(pw: string): StreamableFile {
+    if (pw != this.configService.get('LOG_PW')) {
+      throw new UnauthorizedException('로그 파일을 확인할 권한이 없습니다.');
+    }
     if (!existsSync(this.logFilePath)) {
       throw new NotFoundException('로그 파일이 존재하지 않습니다.');
     }
@@ -18,7 +26,11 @@ export class LogService {
       type: 'text/plain',
     });
   }
-  getErrorLogs(): StreamableFile {
+
+  getErrorLogs(pw: string): StreamableFile {
+    if (pw != this.configService.get('LOG_PW')) {
+      throw new UnauthorizedException('로그 파일을 확인할 권한이 없습니다.');
+    }
     if (!existsSync(this.logFilePath)) {
       throw new NotFoundException('로그 파일이 존재하지 않습니다.');
     }
